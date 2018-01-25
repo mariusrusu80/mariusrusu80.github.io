1129611bcafb7c8b18b821d8f5aa8250875b8eac
mariusrusu80 <32520048+mariusrusu80@users.noreply.github.com>
2018-01-25 03:16:28 +0200

    Update mylib.js

--- mylib.js
+++ mylib.js
@@ -1,22 +1,32 @@
 function afiseazaInformatii() {
-    function updateBatteryStatus(battery) {
-        document.querySelector('#charging').textContent = battery.charging ? 'charging' : 'not charging';
-        document.querySelector('#level').textContent = battery.level;
-        document.querySelector('#dischargingTime').textContent = battery.dischargingTime / 60;
-
+    //actualizeaza automat statusul bateriei si prezinta grafic nivelul bateriei sub forma unei "placinte" 
+    function actualizeazaStatusBaterie(battery) {
+        //actualizeaza continutul din div-urile incarcare, nivelBaterie si timpDescarcare
+        //este conectat incarcatorul sau nu?
+        document.querySelector('#incarcare').textContent = battery.charging ? 'se incarca' : 'nu se incarca';
+        //nivelul bateriei
+        document.querySelector('#nivelBaterie').textContent = battery.level;
+        //timpul de descarcare in minute
+        document.querySelector('#timpDescarcare').textContent = battery.dischargingTime / 60;
+        //creeaza pie chart cu nivelul bateriei
+        //incarcarea - are valori de la 0 pana la 1
         var incarcare = battery.level; 
+        //cat este descarcarea, diferenta dintre 1 si incarcare
         var descarcare = 1-incarcare; 
-        var chart = new CanvasJS.Chart("incarcareBaterie", {
+        //chartul
+        var chart = new CanvasJS.Chart("pieIncarcareBaterie", {
+          //proprietati titlu
           title: {
             text: "Incarcare Baterie",
           },
+          //proprietati legenda
           legend: {
             verticalAlign: "center",
             horizontalAlign: "left",
             fontSize: 15,
             fontFamily: "arial"
           },
-
+          //proprietati "placinta"
           data: [{
             type: "pie",
             percentFormatString: "#0",
@@ -26,34 +36,37 @@ function afiseazaInformatii() {
             indexLabelFontSize: 15,
             indexLabelMaxWidth: 53,   
             indexLabel: "{y} (#percent%)",
+            //incepem de la unghiul 270, pentru a se afisa frumos (de sus)
             startAngle: 270,
             dataPoints: [
+                  //prima este incarcarea
                   { y: incarcare, legendText: 'Baterie incarcata in procentul' }, 
+                  //descarcarea
                   { y: descarcare, legendText: 'Baterie descarcata in procentul' }
             ]
           }]
         });
-
+        //afiseaza pie chart-ul
         chart.render();
-
-
     }
 
+    //vezi https://www.w3.org/TR/battery-status/
     navigator.getBattery().then(function(battery) {
-    // Update the battery status initially when the promise resolves ...
-    updateBatteryStatus(battery);
-
-    // .. and for any subsequent updates.
-    battery.onchargingchange = function () {
-      updateBatteryStatus(battery);
-    };
-
-    battery.onlevelchange = function () {
-      updateBatteryStatus(battery);
-    };
-
-    battery.ondischargingtimechange = function () {
-      updateBatteryStatus(battery);
-    };
+        // actualizeaza statusul bateriei la inceput
+        actualizeazaStatusBaterie(battery);
+        // actualizeaza statusul in cazul in care este conectat / deconectat de la transformator
+        battery.onchargingchange = function () {
+          actualizeazaStatusBaterie(battery);
+        };
+        // actualizeaza statusul in cazul in care se schimba nivelul bateriei
+        battery.onlevelchange = function () {
+          actualizeazaStatusBaterie(battery);
+        };
+        //actualizeaza statusul in cazul in care se schimba timpul de descarcare (in minute)
+        battery.ondischargingtimechange = function () {
+          actualizeazaStatusBaterie(battery);
+        };
     });
 };
+
+
