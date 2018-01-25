addfd3a1f0cbe1ab5cff9ec353db45d76931882c
mariusrusu80 <32520048+mariusrusu80@users.noreply.github.com>
2018-01-25 02:49:02 +0200

    Update mylib.js

--- mylib.js
+++ mylib.js
@@ -1,33 +1,59 @@
 function afiseazaInformatii() {
-    var incarcare = 0.90; 
-    var descarcare = 1-incarcare; 
-    var chart = new CanvasJS.Chart("incarcareBaterie", {
-      title: {
-        text: "Incarcare Baterie",
-      },
-      legend: {
-        verticalAlign: "center",
-        horizontalAlign: "left",
-        fontSize: 15,
-        fontFamily: "arial"
-      },
+    function updateBatteryStatus(battery) {
+        document.querySelector('#charging').textContent = battery.charging ? 'charging' : 'not charging';
+        document.querySelector('#level').textContent = battery.level;
+        document.querySelector('#dischargingTime').textContent = battery.dischargingTime / 60;
 
-      data: [{
-        type: "pie",
-        percentFormatString: "#0",
-        toolTipContent: "{legendText} (#percent%)",
-        showInLegend: true,
-        indexLabelPlacement: "outside",    
-        indexLabelFontSize: 15,
-        indexLabelMaxWidth: 53,   
-        indexLabel: "{y} (#percent%)",
-        startAngle: 270,
-        dataPoints: [
-              { y: incarcare, legendText: 'Baterie incarcata in procentul' }, 
-              { y: descarcare, legendText: 'Baterie descarcata in procentul' }
-        ]
-      }]
+        var incarcare = battery.level; 
+        var descarcare = 1-incarcare; 
+        var chart = new CanvasJS.Chart("incarcareBaterie", {
+          title: {
+            text: "Incarcare Baterie",
+          },
+          legend: {
+            verticalAlign: "center",
+            horizontalAlign: "left",
+            fontSize: 15,
+            fontFamily: "arial"
+          },
+
+          data: [{
+            type: "pie",
+            percentFormatString: "#0",
+            toolTipContent: "{legendText} (#percent%)",
+            showInLegend: true,
+            indexLabelPlacement: "outside",    
+            indexLabelFontSize: 15,
+            indexLabelMaxWidth: 53,   
+            indexLabel: "{y} (#percent%)",
+            startAngle: 270,
+            dataPoints: [
+                  { y: incarcare, legendText: 'Baterie incarcata in procentul' }, 
+                  { y: descarcare, legendText: 'Baterie descarcata in procentul' }
+            ]
+          }]
+        });
+
+        chart.render();
+
+
+    }
+
+    navigator.getBattery().then(function(battery) {
+    // Update the battery status initially when the promise resolves ...
+    updateBatteryStatus(battery);
+
+    // .. and for any subsequent updates.
+    battery.onchargingchange = function () {
+      updateBatteryStatus(battery);
+    };
+
+    battery.onlevelchange = function () {
+      updateBatteryStatus(battery);
+    };
+
+    battery.ondischargingtimechange = function () {
+      updateBatteryStatus(battery);
+    };
     });
-
-    chart.render();
-}
+};
